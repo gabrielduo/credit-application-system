f299193206237368110f4f9db9248e92ef3e932e
Gabriel Duarte <142861188+gabrielduo@users.noreply.github.com>
2023-10-28 00:46:04 -0300

    Implementação das regras de negócio para solicitação de empréstimo

--- src/main/kotlin/me/dio/credit/application/system/service/impl/CreditService.kt
+++ src/main/kotlin/me/dio/credit/application/system/service/impl/CreditService.kt
@@ -11,35 +11,39 @@ import java.util.*
 
 @Service
 class CreditService(
-  private val creditRepository: CreditRepository,
-  private val customerService: CustomerService
+    private val creditRepository: CreditRepository,
+    private val customerService: CustomerService
 ) : ICreditService {
-  override fun save(credit: Credit): Credit {
-    this.validDayFirstInstallment(credit.dayFirstInstallment)
-    credit.apply {
-      customer = customerService.findById(credit.customer?.id!!)
+
+    override fun save(credit: Credit): Credit {
+        validateNumberOfInstallments(credit.numberOfInstallments)
+        validDayFirstInstallment(credit.dayFirstInstallment)
+        credit.apply {
+            customer = customerService.findById(credit.customer?.id!!)
+        }
+
+        return this.creditRepository.save(credit)
     }
-    return this.creditRepository.save(credit)
-  }
 
-  override fun findAllByCustomer(customerId: Long): List<Credit> =
-    this.creditRepository.findAllByCustomerId(customerId)
+    override fun findAllByCustomer(customerId: Long): List<Credit> =
+        this.creditRepository.findAllByCustomerId(customerId)
 
-  override fun findByCreditCode(customerId: Long, creditCode: UUID): Credit {
-    val credit: Credit = (this.creditRepository.findByCreditCode(creditCode)
-      ?: throw BusinessException("Creditcode $creditCode not found"))
-    return if (credit.customer?.id == customerId) credit
-    else throw IllegalArgumentException("Contact admin")
-    /*if (credit.customer?.id == customerId) {
-      return credit
-    } else {
-      throw RuntimeException("Contact admin")
-    }*/
-  }
+    override fun findByCreditCode(customerId: Long, creditCode: UUID): Credit {
+        val credit: Credit = (this.creditRepository.findByCreditCode(creditCode)
+            ?: throw BusinessException("Creditcode $creditCode not found"))
+        return if (credit.customer?.id == customerId) credit
+        else throw IllegalArgumentException("Contact admin")
+    }
 
-  private fun validDayFirstInstallment(dayFirstInstallment: LocalDate): Boolean {
-    return if (dayFirstInstallment.isBefore(LocalDate.now().plusMonths(3))) true
-    else throw BusinessException("Invalid Date")
-  }
+    private fun validateNumberOfInstallments(numberOfInstallments: Int) {
+        if (numberOfInstallments > 48) {
+            throw BusinessException("Número máximo de parcelas excedido")
+        }
+    }
+
+    private fun validDayFirstInstallment(dayFirstInstallment: LocalDate) {
+        if (!dayFirstInstallment.isBefore(LocalDate.now().plusMonths(3))) {
+            throw BusinessException("A data da primeira parcela deve ser no máximo 3 meses após a data atual.")
+        }
+    }
 }
-
